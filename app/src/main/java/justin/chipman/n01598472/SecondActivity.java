773ba2003df188556d0afa878efc86e60f3b6423
JustinChipman8472 <n01598472@humber.ca>
2024-02-07 00:59:28 -0500

    added the textview on the second actiivity that will have the data passed to it

--- app/src/main/java/justin/chipman/n01598472/SecondActivity.java
+++ app/src/main/java/justin/chipman/n01598472/SecondActivity.java
@@ -1,8 +1,9 @@
 package justin.chipman.n01598472;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.TextView;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +11,10 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        long data = getIntent().getLongExtra(getString(R.string.userinput), -1000);
+        Log.d("SecondActivity","Recieved data: " + data);
+        TextView textView = findViewById(R.id.textView); // Assume you have a TextView to display the data
+        textView.setText(String.valueOf(data));
     }
 }

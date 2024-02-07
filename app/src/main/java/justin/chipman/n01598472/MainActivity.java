2600b2b56f205f79c2da95031315977d06cfe2e3
JustinChipman8472 <n01598472@humber.ca>
2024-02-06 23:46:36 -0500

    Added second activity and a button to the main activity

--- app/src/main/java/justin/chipman/n01598472/MainActivity.java
+++ app/src/main/java/justin/chipman/n01598472/MainActivity.java
@@ -1,8 +1,12 @@
 package justin.chipman.n01598472;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.snackbar.Snackbar;
 // Justin Chipman n01598472
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }

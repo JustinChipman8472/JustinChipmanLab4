3f27935452e870a921571340e563f4df71d7cedc
JustinChipman8472 <n01598472@humber.ca>
2024-02-07 00:02:04 -0500

    Added specified functionality to button so that it passes the data from the edittext field

--- app/src/main/java/justin/chipman/n01598472/MainActivity.java
+++ app/src/main/java/justin/chipman/n01598472/MainActivity.java
@@ -15,6 +15,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button displayButton = findViewById(R.id.displayButton);
+        final EditText inputEditText = findViewById(R.id.inputEditText);
+
+        displayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = inputEditText.getText().toString();
+                long data = input.isEmpty() ? -1000 : Long.parseLong(input);
+
+                // Show Snackbar
+                Snackbar snackbar = Snackbar
+                        .make(view, getString(R.string.my_name), Snackbar.LENGTH_INDEFINITE)
+                        .setAction(R.string.start, new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                // Start SecondActivity and pass the data
+                                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                                intent.putExtra(getString(R.string.userinput), data);
+                                startActivity(intent);
+                            }
+                        });
+                snackbar.show();
+            }
+        });
 
     }
 }
